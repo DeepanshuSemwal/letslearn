de6b2acabd317aa4c516b5b8045c5afeffe00ae2
Deepanshu <semwaldeepanshu@gmail.com>
2022-03-04 21:11:23 +0530

    added multiply

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+package letslearn;
+
+public class Multiply {
+    public static void main(String[] args)
+    {
+        int a=5;
+        int b=7;
+        System.out.print("multiplication of a*b is "+a*b);
+    }
+    
+}
